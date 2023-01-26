39579a2992b1ef3fb76c3b6427a886df5e1b5668
FadliGr1 <fadliblod00@gmail.com>
2023-01-26 09:22:40 +0700

    pelanggan

--- js/index.js
+++ js/index.js
@@ -1,31 +1,4 @@
-// const mode = {
-//   dark: "Dark Mode",
-//   light: "Light Mode",
-// };
-
-// const checkbox = document.getElementById("checkbox");
-
-// const theme = localStorage.getItem("theme");
-// document.body.classList.add(theme || null);
-// checkbox.checked = theme !== "dark";
-
-// document.getElementById("label-mode").innerText = mode[theme || "light"];
-
-// checkbox.addEventListener("change", () => {
-//   document.body.classList.toggle("dark");
-
-//   const theme = localStorage.getItem("theme");
-//   checkbox.checked = theme === "dark";
-
-//   if (theme === "dark") {
-//     localStorage.setItem("theme", "");
-//     document.getElementById("label-mode").innerText = mode.light;
-//   } else {
-//     localStorage.setItem("theme", "dark");
-//     document.getElementById("label-mode").innerText = mode.dark;
-//   }
-// });
-
+// side bar
 function handleFavorite(e) {
   const btnFav = e.closest(".btn-fav");
   btnFav.classList.toggle("active");
@@ -55,6 +28,7 @@ function handleClickMenu(e) {
   e.closest(".item-menu").classList.add("active");
 }
 
+// calender
 const currentDate = document.querySelector(".current-date"),
   daysTag = document.querySelector(".days"),
   prevNextIcon = document.querySelectorAll(".icons span");
